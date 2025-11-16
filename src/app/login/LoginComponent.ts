5d34ed47eadef4f462b4a4cf0443d6f16f4cd1a3
akocoglu <ahmetkocoglu@outlook.com>
2025-11-16 17:24:38 +0300

    login

---
+++ src/app/login/LoginComponent.ts
@@ -0,0 +1,57 @@
+
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+  errorMessage: string = '';
+  isLoading: boolean = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.isLoading = true;
+      this.errorMessage = '';
+      
+      const { email, password } = this.loginForm.value;
+      
+      // TODO: Burada gerçek API çağrısı yapılacak
+      console.log('Login attempt:', { email, password });
+      
+      // Simüle edilmiş login işlemi
+      setTimeout(() => {
+        this.isLoading = false;
+        // Başarılı login sonrası yönlendirme
+        // this.router.navigate(['/dashboard']);
+      }, 1000);
+    } else {
+      this.errorMessage = 'Lütfen tüm alanları doğru şekilde doldurun.';
+    }
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+}
